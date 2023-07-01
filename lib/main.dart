86a7ec44bc99e527712b94332d24db428b1c4582
Jerry-Banga <102557931+Jerry-Banga@users.noreply.github.com>
2023-07-01 20:14:40 +0300

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: Scaffold(
+        backgroundColor: Colors.white,
         body: SafeArea(
           child: Center(
             child: IconButton(
